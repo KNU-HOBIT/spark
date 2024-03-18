f13042f74506bb4c3e4cadd3f9b5441ea2336f4f
Yusu Noh <noyusu@knu.ac.kr>
2024-03-18 21:11:39 +0900

    test

--- read-dataset.py
+++ read-dataset.py
@@ -5,7 +5,6 @@ import os
 
 spark = SparkSession.builder \
         .appName("test") \
-        .config("spark.jars", "/root/.ivy2/jars/*") \
         .getOrCreate() \
 
 spark.sparkContext.setLogLevel('WARN')
